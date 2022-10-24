006dba66eca15080718ab3c4f616d0c9f420f057
kw22228 <hot5521@naver.com>
2022-10-24 18:01:19 +0900

    1024

--- Javascript/모던 자바스크립트/19. 프로토타입/index.js
+++ Javascript/모던 자바스크립트/19. 프로토타입/index.js
@@ -1,12 +1,28 @@
-const person = {
-    name: 'kim',
-    address: 'seoul',
+// const person = {
+//     name: 'kim',
+//     address: 'seoul',
+// };
+
+// const obj = new Object();
+
+const func = function (value, value2) {
+    this.val = value;
+    this.val2 = value2;
 };
 
-const func = function () {
-    return 'a';
-};
-
-const f = new func();
+const f = new func('aaa', 'bbb');
 
 console.dir(func);
+console.dir(f);
+// console.dir(person);
+// console.dir(obj);
+
+console.log('======================\n');
+
+const obj = {};
+const parent = { x: 1 };
+
+// obj.__proto__ = parent;
+
+Object.setPrototypeOf(obj, parent);
+console.log(obj);
