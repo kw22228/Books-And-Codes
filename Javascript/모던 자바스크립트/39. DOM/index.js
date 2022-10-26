7e598ab482c45d0c1199d095b508a9ab0ec23a45
kw22228 <hot5521@naver.com>
2022-10-26 17:29:11 +0900

    20221026

---
+++ Javascript/모던 자바스크립트/39. DOM/index.js
@@ -0,0 +1,9 @@
+// const $element = document.getElementById('banana');
+// $element.style.color = 'red';
+
+const $element = document.getElementById('fruits');
+console.log($element);
+const { firstChild } = $element;
+console.log(firstChild);
+
+console.log(DOMPurify);
