24d7a0c3f0f992ac661189001416b96f612b7070
김재원 <kw2282222@gmail.com>
2023-01-05 23:31:08 +0900

    0105

--- Typescript/이펙티브_타입스크립트/chapter/2. 타입스크립트_타입_시스템/src/아이템18(매핑된_타입을_사용하여_값을_동기화하기)/index.ts
+++ Typescript/이펙티브_타입스크립트/chapter/2. 타입스크립트_타입_시스템/src/아이템18(매핑된_타입을_사용하여_값을_동기화하기)/index.ts
@@ -1,55 +1,55 @@
 interface ScatterProps {
-    xs: number[];
-    ys: number[];
+  xs: number[];
+  ys: number[];
 
-    xRange: [number, number];
-    yRange: [number, number];
-    color: string;
+  xRange: [number, number];
+  yRange: [number, number];
+  color: string;
 
-    onClick: (x: number, y: number, index: number) => void;
-    // onDoubleClick: () => void;
+  onClick: (x: number, y: number, index: number) => void;
+  onDoubleClick: () => void;
 }
 
 //onClick을 제외한 props가 변하면 업데이트를 진행함.
 function shouldUpdate(oldProps: ScatterProps, newProps: ScatterProps) {
-    let k: keyof ScatterProps;
-    for (k in oldProps) {
-        if (oldProps[k] !== newProps[k]) {
-            if (k !== 'onClick') return true;
-        }
+  let k: keyof ScatterProps;
+  for (k in oldProps) {
+    if (oldProps[k] !== newProps[k]) {
+      if (k !== 'onClick') return true;
     }
+  }
 
-    return false;
+  return false;
 }
 
 function shouldUpdate2(oldProps: ScatterProps, newProps: ScatterProps) {
-    return (
-        oldProps.xs !== newProps.xs ||
-        oldProps.ys !== newProps.ys ||
-        oldProps.xRange !== newProps.xRange ||
-        oldProps.yRange !== newProps.yRange ||
-        oldProps.color !== newProps.color
+  return (
+    oldProps.xs !== newProps.xs ||
+    oldProps.ys !== newProps.ys ||
+    oldProps.xRange !== newProps.xRange ||
+    oldProps.yRange !== newProps.yRange ||
+    oldProps.color !== newProps.color
 
-        // no check for onClick
-    );
+    // no check for onClick
+  );
 }
 
 //////////////////////////////////////
 
 const REQUEST_UPDATE: { [k in keyof ScatterProps]: boolean } = {
-    xs: true,
-    ys: true,
-    xRange: true,
-    yRange: true,
-    color: true,
-    onClick: false,
+  xs: true,
+  ys: true,
+  xRange: true,
+  yRange: true,
+  color: true,
+  onClick: false,
 };
 
 function shouldUpdate3(oldProps: ScatterProps, newProps: ScatterProps) {
-    let k: keyof ScatterProps;
-    for (k in oldProps) {
-        if (oldProps[k] !== newProps[k] && REQUEST_UPDATE[k]) return true;
-    }
+  let k: keyof ScatterProps;
+  for (k in oldProps) {
+    if (oldProps[k] !== newProps[k] && REQUEST_UPDATE[k]) return true;
+  }
 
-    return false;
+  return false;
 }
