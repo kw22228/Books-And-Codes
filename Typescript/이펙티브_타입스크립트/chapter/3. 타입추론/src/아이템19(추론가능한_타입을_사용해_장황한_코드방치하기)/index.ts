7428c3de39b5f76f982b8246efffdc8f48274f54
@kimjaewon <hot5521@naver.com>
2023-01-10 17:49:47 +0900

    0110

---
+++ Typescript/이펙티브_타입스크립트/chapter/3. 타입추론/src/아이템19(추론가능한_타입을_사용해_장황한_코드방치하기)/index.ts
@@ -0,0 +1,155 @@
+import express from 'express';
+
+(function () {
+    //프로젝트내 변수 공유 체크로 인한 지역변수 사용.
+
+    /** 변수 타입추론 (type declaration을 안해줘도 자동으로 잡혀있음.) */
+    let xWithType: number = 12;
+    let x = 12;
+
+    /** 객체 타입추론 */
+    const personWithType: {
+        name: string;
+        born: {
+            where: string;
+            when: string;
+        };
+        died: {
+            where: string;
+            when: string;
+        };
+    } = {
+        name: 'Sojourner Truth',
+        born: {
+            where: 'Swartekill, NY',
+            when: 'c.1797',
+        },
+        died: {
+            where: 'Battle Creek, MI',
+            when: 'Nov, 26, 1883',
+        },
+    };
+
+    const person = {
+        name: 'Sojourner Truth',
+        born: {
+            where: 'Swartekill, NY',
+            when: 'c.1797',
+        },
+        died: {
+            where: 'Battle Creek, MI',
+            when: 'Nov, 26, 1883',
+        },
+    };
+
+    /** 배열의 타입추론 */
+    function square(nums: number[]) {
+        return nums.map(x => x * x);
+    }
+    const squares = square([1, 2, 3, 4]);
+
+    /////////////////////////////////////////////
+
+    const axis1: string = 'x'; // string
+    const axis2 = 'y'; // 'y'
+
+    interface Product {
+        id: string;
+        name: string;
+        price: number;
+    }
+    function logProduct(product: Product) {
+        const id: number = product.id; //타입선언으로 인해 추론이 되어지지않아 오류가 나고있음.
+        const name: string = product.name;
+        const price: number = product.price;
+        console.log(id, name, price);
+    }
+
+    function logProduct2(product: Product) {
+        // const { id, name, price } = product; // 알아서 타입추론됨.
+        const { id, name, price }: { id: string; name: string; price: number } = product; //아주 번잡한 코드가 되어버림.
+        console.log(id, name, price);
+    }
+
+    /** 매개변수에 타입구문을 생략하는 경우 */
+    function parseNumber(str: string, base = 10) {} //base 매개변수의 default값이 10(number)이기 떄문에 알아서 추론됨.
+
+    const app = express();
+    //Bad
+    app.get('/health', (request: express.Request, response: express.Response) => {
+        response.send('OK');
+    });
+    //Good
+    app.get('/health', (request, response) => {
+        response.send('OK');
+    });
+
+    /** 타입추론이 되는데도 타입을 명시하고 싶은 경우 */
+    const elmo: Product = {
+        name: 'Tickle Me Elmo',
+        id: '0481812 2940193',
+        price: 28.99,
+    }; //잉여 속성 체크가 동작함.
+
+    const furby = {
+        name: 'Furby',
+        id: 123121353,
+        price: 35,
+    };
+    logProduct2(furby);
+    const furbyWithType: Product = {
+        name: 'Furby',
+        id: 123121353, //string이여야함
+        price: 35,
+    };
+
+    function getQuote(ticker: string) {
+        return fetch(`https://quotes.example.com/?q=${ticker}`).then(response => response.json());
+    }
+
+    const cache: { [ticker: string]: number } = {};
+    function getQuoteWithoutReturnType(ticker: string) {
+        if (ticker in cache) {
+            return cache[ticker]; //number타입으로 return
+        }
+        return fetch(`https://quotes.example.com/?q=${ticker}`)
+            .then(response => response.json())
+            .then(quote => {
+                cache[ticker] = quote;
+                return quote;
+            });
+    }
+    getQuoteWithoutReturnType('MSFT').then('considerBuying');
+
+    function getQuoteWithReturnType(ticker: string): Promise<number> {
+        if (ticker in cache) {
+            return Promise.resolve(cache[ticker]); //그냥 number타입이기때문에 Promise<number>에 들어올 수 없음.
+        }
+        return fetch(`https://quotes.example.com/?q=${ticker}`)
+            .then(response => response.json())
+            .then(quote => {
+                cache[ticker] = quote;
+                return quote;
+            });
+    }
+    getQuoteWithReturnType('MSFT').then(console.log);
+
+    // 명명된 반환 타입
+    interface Vector2D {
+        x: number;
+        y: number;
+    }
+    function add(a: Vector2D, b: Vector2D) {
+        return {
+            x: a.x + b.x,
+            y: a.y + b.y,
+        };
+    } // 반환타입이 Vector2D가 아니라 {x: number, y: number}이다. (명명되지 않음)
+})();
+
+/*
+결론
+1. 타입스크립트가 타입추론을 할 수 있다면, 굳이 타입구문을 작성할 필요가 없다.
+2. 이상적인 경우는 함수/메서드의 시그니처에는 타입구문이 있지만, 함수내의 지역변수에는 타입 구문이 없다.
+3. 추론될 수 있는 경우라도 객체 리터럴과 함수 반환에는 타입명시를 고려해야 한다. 
+*/
---
+++ Typescript/이펙티브_타입스크립트/chapter/3. 타입추론/src/아이템22(타입_좁히기)/index.ts
@@ -0,0 +1,124 @@
+(function () {
+    //프로젝트내 변수 공유 체크로 인한 지역변수 사용.
+
+    const el = document.getElementById('foo');
+
+    /** 분기문의 따른 좁히기. */
+    if (el) {
+        //htmlElement | null 에서 null을 제외하므로 더 좁은 타입체커가 되엇다.
+        el.innerHTML = 'Party Time'.blink();
+    } else {
+        el;
+        alert('No element');
+    }
+
+    /** error를 통한 좁히기 */
+    if (!el) throw new Error('Unable to find #foo');
+    el; //null제외
+
+    /** early return을 통한 좁히기 */
+    function contains(text: string, search: string | RegExp) {
+        if (search instanceof RegExp) {
+            search;
+            return !!search.exec(text);
+        }
+
+        //early return으로 인해 search가 string타입으로 좁혀졋다
+        search;
+        return text.includes(search);
+    }
+
+    /** 속성 체크로 인한 좁히기 */
+    interface A {
+        a: number;
+    }
+    interface B {
+        b: number;
+    }
+    function pickAB(ab: A | B) {
+        if ('a' in ab) ab;
+        else ab;
+
+        ab;
+    }
+
+    /** 빌트인 메서드로 인한 좁히기 */
+    function myFunc(text: string, terms: string | string[]) {
+        const termList = Array.isArray(terms) ? terms : [terms]; // 무조건 array타입으로 좁혀짐
+    }
+
+    ////////////////////////////////////////////
+
+    /** 실수를 저지르기 쉬운 예제 */
+    const el2 = document.querySelector('foo');
+    if (typeof el2 === 'object') {
+        el2; //null일때도 object이다.
+    }
+
+    function foo(x?: number | string | null) {
+        if (!x) {
+            x; // '', 0, 모두 false이다.
+        }
+    }
+
+    /////////////////////////////////////////////////
+    /** 일반적인 타입 좁히는 방법 (명시적 태그를 붙임)*/
+
+    //1. 태그된 유니온 (구별된 유니온) 패턴
+    interface UploadEvent {
+        type: 'upload';
+        filename: string;
+        contents: string;
+    }
+    interface DownloadEvent {
+        type: 'download';
+        filename: string;
+    }
+    type AppEvent = UploadEvent | DownloadEvent;
+    function handleEvent(e: AppEvent) {
+        // 각 interface의 unit타입을 이용해 타입을 좁혓다.
+        switch (e.type) {
+            case 'download':
+                e; //DownloadEvent
+                break;
+            case 'upload':
+                e; //UploadEvent
+                break;
+        }
+    }
+
+    //2. 사용자정의 타입 가드
+    function isInputElement(el: HTMLElement): el is HTMLInputElement {
+        // el is HTMLInputElement -> 함수의 반환이 true일경우 HTMLInputElement 이다.
+        return 'value' in el;
+    }
+    function getElementContent(el: HTMLElement) {
+        if (isInputElement(el)) {
+            el;
+            return el.value;
+        }
+        el;
+        return el.textContent;
+    }
+
+    //3. 타입가드를 이용한 배열과 객체의 타입 좁히기
+    const jackson5 = ['Jackie', 'Tito', 'Jermaine', 'Marlon', 'Michael'];
+    const members = ['Janet', 'Michael']
+        .map(who => jackson5.find(n => n === who))
+        .filter(who => who !== undefined);
+
+    function isDefined<T>(x: T | undefined): x is T {
+        return x !== undefined;
+    }
+
+    // ******* 이거의 차이 매우 중요한듯(filter의 callback함수 동작 방식이 다름) **********
+    const members2 = ['Janet', 'Michael']
+        .map(who => jackson5.find(n => n === who))
+        .filter(x => isDefined(x));
+    const members3 = ['Janet', 'Michael']
+        .map(who => jackson5.find(n => n === who)) // string|undefined []
+        .filter(isDefined);
+})();
+/*
+
+*/
