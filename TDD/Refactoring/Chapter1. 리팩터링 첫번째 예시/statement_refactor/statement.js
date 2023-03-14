d072230839fa732fa9a22423c651afceb9e2d04b
@kimjaewon <hot5521@naver.com>
2023-03-14 13:20:01 +0900

    refactor: Intl nuber format -> 함수 추출하기

--- TDD/Refactoring/Chapter1. 리팩터링 첫번째 예시/statement_refactor/statement.js
+++ TDD/Refactoring/Chapter1. 리팩터링 첫번째 예시/statement_refactor/statement.js
@@ -1,4 +1,5 @@
 import amountFor from './amountFor';
+import usd from './usd'; // format이라고 하기에는 너무 명확하지 못하기 때문에 usd로 바꿈.
 import playFor from './playFor';
 import volumeCreditsFor from './volumeCreditsFor';
 
@@ -13,21 +14,16 @@ function statement(invoice, plays) {
   let totalAmount = 0; //총액
   let volumeCredits = 0; //포인트
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
     //청구 내역 출력
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
 
   return result;
---
+++ TDD/Refactoring/Chapter1. 리팩터링 첫번째 예시/statement_refactor/usd.js
@@ -0,0 +1,11 @@
+/** number Formatter 함수 (USD currency) */
+function usd(aNumber) {
+  const options = {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  };
+  return new Intl.NumberFormat('en-US', options).format(aNumber / 100); // 단위 변환 로직 추가
+}
+
+export default usd;
