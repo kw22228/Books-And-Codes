82f2eafabfdd5eb707e5fbe3f5f939d0ba0370df
@kimjaewon <hot5521@naver.com>
2023-03-22 17:31:37 +0900

    Chapter3. 코드에서 나는 악취

---
+++ TDD/Refactoring/Chapter3. 코드에서 나는 악취/messageChain.js
@@ -0,0 +1,6 @@
+let managerName = aPerson.department.manager.name;
+managerName = aPerson.department.managerName;
+managerName = aPerson.manager.name;
+managerName = aPerson.managerName;
+
+managerName = aPerson.department.manager.name;
