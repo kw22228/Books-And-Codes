6621276d38f53e479d59b910a4d583932c8ba37c
@kimjaewon <hot5521@naver.com>
2023-08-31 17:32:40 +0900

    refactor

---
+++ Programming/파이스 라인스 코드/Chapter2. 리팩터링 깊게 들여다보기/index.ts
@@ -0,0 +1,13 @@
+function checkValue(str: boolean) {
+  //값 체크
+
+  if (str !== false) return true;
+  else;
+
+  return str;
+}
+
+function isTrue(bool: boolean) {
+  if (bool) return true;
+  else false;
+}
