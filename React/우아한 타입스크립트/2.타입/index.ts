e26d8f16a8cc281c32a7ea9c93921d5fcaf3ca65
@kimjaewon <hot5521@naver.com>
2024-06-03 17:25:55 +0900

    types

--- React/우아한 타입스크립트/2.타입/index.ts
+++ React/우아한 타입스크립트/2.타입/index.ts
@@ -1,4 +1,20 @@
 var name = 'zig';
 var year = 2022;
 
+const num: number = 123;
+const str: string = 'abc';
+
+function func(n: number) {
+  return n;
+}
+
+func(num);
+console.log(func(str));
+
+function double(n: number) {
+  return n * 2;
+}
+
+double(2);
+double(`z`);
 export {};
